4857bec266cb0b8b3d55e2104aa9e33518184a25
ProfessorZel <rabota1schola11@gmail.com>
2025-08-23 01:17:34 +0300

    Изменен сопособ формирования заголовков

--- bot/operations/list_users.py
+++ bot/operations/list_users.py
@@ -1,23 +1,9 @@
-import json
-import os
-
 import ldap3
-from ldap3 import Connection
 from ldap3.core.exceptions import LDAPException
 
-from common import ldap
+from common.config import settings
 from common.ldap import get_connection
 
-ad_bind_user = os.getenv("AD_BIND_USER")
-ad_bind_pass = os.getenv("AD_BIND_PASSWORD")
-domain = os.getenv("DOMAIN")
-base_dn = os.getenv("BASE_DN")
-user_search_base = os.getenv("USER_SEARCH_BASE")
-group_search_base = os.getenv("GROUP_SEARCH_BASE")
-user_group_mapping = json.loads(os.getenv("USER_GROUP_MAPPING"))
-user_ou_mapping = json.loads(os.getenv("USER_OU_MAPPING"))
-mail_domain = os.getenv("MAIL_DOMAIN")
-temp_password = os.getenv("NEW_USER_TEMP_PASS")
 
 def get_users_by_ou() -> dict:
     """
@@ -33,16 +19,10 @@ def get_users_by_ou() -> dict:
     }
 
     try:
-        # Получаем уникальные OU из маппинга
-        unique_ous = set(user_ou_mapping.values())
-
-        # Инициализируем структуру для результатов
-        result['users_by_ou'] = {ou: [] for ou in unique_ous}
-
         conn = get_connection()
-        for ou_path in unique_ous:
-            search_base = f"{ou_path},{base_dn}"
-
+        for name, ou_path in dict(settings.user_ou_mapping).items():
+            search_base = f"{ou_path},{settings.base_dn}"
+            result['users_by_ou'][name] = []
             # Ищем всех пользователей в текущем OU
             conn.search(
                 search_base=search_base,
@@ -56,7 +36,7 @@ def get_users_by_ou() -> dict:
                     'sAMAccountName': entry.sAMAccountName.value if 'sAMAccountName' in entry else None,
                     'displayName': entry.displayName.value if 'displayName' in entry else None
                 }
-                result['users_by_ou'][ou_path].append(user_info)
+                result['users_by_ou'][name].append(user_info)
 
         result['success'] = True
         result['message'] = f"Найдено пользователей: {sum(len(users) for users in result['users_by_ou'].values())}"
